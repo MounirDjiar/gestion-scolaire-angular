8227ad902bdc1cda9af729387fdcfa35207b01a3
Mounir DJIAR <mounir@djiar.com>
2023-02-23 16:38:52 +0100

    Rajout des services school pour recuperer les classrooms, matieres, teachers, clazzs par etablissement

--- src/app/components/school-details/school-details.component.ts
+++ src/app/components/school-details/school-details.component.ts
@@ -2,6 +2,10 @@ import {Component, OnInit} from '@angular/core';
 import {School} from "../../../models/school.model";
 import {SchoolService} from "../../../services/school.service";
 import {ActivatedRoute} from "@angular/router";
+import {Classroom} from "../../../models/classroom.model";
+import {Lesson} from "../../../models/lesson.model";
+import {Clazz} from "../../../models/clazz.model";
+import {Teacher} from "../../../models/teacher.model";
 
 @Component({
   selector: 'app-school-details',
@@ -11,16 +15,15 @@ import {ActivatedRoute} from "@angular/router";
 export class SchoolDetailsComponent implements OnInit {
 
   school!: School;
-
-  nbProfesseurs: number = 0;
-  nbMatieres: number = 0;
-  nbClazzs: number = 0;
-  nbClassrooms: number = 0;
+  classroomsList: Classroom[] = [];
+  lessonsList: Lesson[] = [];
+  clazzsList: Clazz[] = [];
+  teachersList: Teacher[] = [];
 
   constructor(
       private schoolService: SchoolService,
       private activateRoute: ActivatedRoute,
-  ) {
+   ){
   }
 
   ngOnInit(): void {
@@ -29,7 +32,27 @@ export class SchoolDetailsComponent implements OnInit {
       this.schoolService.findById(Number(id)).subscribe(
         school => {
           this.school = school;
-        })
+        });
+
+      this.schoolService.findClassroomsBySchoolId(Number(id)).subscribe(
+        classrooms => {
+          this.classroomsList = classrooms;
+        });
+
+      this.schoolService.findLessonsBySchoolId(Number(id)).subscribe(
+        lessonsList => {
+          this.lessonsList = lessonsList;
+        });
+
+      this.schoolService.findClazzsBySchoolId(Number(id)).subscribe(
+        clazzsList => {
+          this.clazzsList = clazzsList;
+        });
+
+      this.schoolService.findTeachersBySchoolId(Number(id)).subscribe(
+        teachersList => {
+          this.teachersList = teachersList;
+        });
     }
   }
 }
--- src/services/school.service.ts
+++ src/services/school.service.ts
@@ -2,8 +2,10 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {School} from "../models/school.model";
-
-
+import {Classroom} from "../models/classroom.model";
+import {Lesson} from "../models/lesson.model";
+import {Clazz} from "../models/clazz.model";
+import {Teacher} from "../models/teacher.model";
 
 @Injectable({
   providedIn: 'root'
@@ -24,6 +26,22 @@ export class SchoolService {
     return this.http.get<School>(`${this.url}/${id}`);
   }
 
+  public findClassroomsBySchoolId(id: number): Observable<Classroom[]> {
+    return this.http.get<Classroom[]>(`${this.url}/${id}/classrooms`);
+  }
+
+  public findLessonsBySchoolId(id: number): Observable<Lesson[]> {
+    return this.http.get<Lesson[]>(`${this.url}/${id}/lessons`);
+  }
+
+  public findClazzsBySchoolId(id: number): Observable<Clazz[]> {
+    return this.http.get<Clazz[]>(`${this.url}/${id}/clazzs`);
+  }
+
+  public findTeachersBySchoolId(id: number): Observable<Teacher[]> {
+    return this.http.get<Teacher[]>(`${this.url}/${id}/teachers`);
+  }
+
   public addSchool(value: School): Observable<School> {
     return this.http.post<School>(this.url, value);
   }
@@ -31,5 +49,4 @@ export class SchoolService {
   public deleteSchool(id: number): Observable<void> {
     return this.http.delete<void>(`${this.url}/${id}`);
   }
-
 }
