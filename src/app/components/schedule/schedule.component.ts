0837aa387f5e1eff2164be09148bf1d3c79238bf
NicolasDEQUIDT <nicolas.dequidt@hotmail.com>
2023-02-23 12:43:26 +0100

    Ajout des heures sur le coté
    A améliorer pour mettre que les jours et heures pertinentes

--- src/app/components/schedule/schedule.component.ts
+++ src/app/components/schedule/schedule.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CalendarOptions } from '@fullcalendar/core'; // useful for typechecking
 import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
 
 @Component({
   selector: 'app-schedule',
@@ -9,9 +10,39 @@ import dayGridPlugin from '@fullcalendar/daygrid';
 })
 export class ScheduleComponent {
   calendarOptions: CalendarOptions = {
-    initialView: 'dayGridWeek',
-    hiddenDays: [0],
-    plugins: [dayGridPlugin]
+    initialView: 'timeGridWeek',
+    titleFormat: { year: 'numeric', month: 'short', day: 'numeric', weekday: 'short' },
+    slotDuration: '01:00:00',
+    slotLabelInterval: '01:00:00',
+    slotLabelFormat: { hour: 'numeric', minute: '2-digit', hour12: true },
+    dayHeaders: true,
+    height: 'auto',
+    plugins: [timeGridPlugin],
+    events: [
+      {
+        title: 'Cours de Physique avec les 6ème B',
+        start: '2023-03-06T09:00:00',
+        end: '2023-03-06T12:00:00',
+        extendedProps: {
+          salle: 'B102',
+          professeur: 'Martin'
+        }
+      }
+      // liste des événements
+    ]
+
   };
 
 }
+
+/*
+    initialView: 'timeGrid',
+    columnHeader: false,
+    titleFormat: { year: 'numeric', month: 'short', day: 'numeric', weekday: 'short' },
+    slotDuration: '01:00:00',
+    slotLabelInterval: '01:00:00',
+    slotLabelFormat: { hour: 'numeric', minute: '2-digit', hour12: true },
+    weekends: false,
+    allDaySlot: false,
+    height: 'auto'
+    */
