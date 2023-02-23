2897014b1b0bb9c0e4d69a6b56a084f088822e0d
LeiCla <leila.claveau@hotmail.com>
2023-02-23 13:12:19 +0000

    'details et forms de classe'

--- src/app/components/clazz-details/clazz-details.component.ts
+++ src/app/components/clazz-details/clazz-details.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Classroom} from "../../../models/classroom.model";
+import {NgbModal, NgbModalRef} from "@ng-bootstrap/ng-bootstrap";
+import {ActivatedRoute, Router} from "@angular/router";
+import {ClassroomService} from "../../../services/classroom.service";
+import {environment} from "../../../environments/environment.development";
+import {Clazz} from "../../../models/clazz.model";
+import {ClazzService} from "../../../services/clazz.service";
 
 @Component({
   selector: 'app-clazz-details',
   templateUrl: './clazz-details.component.html',
   styleUrls: ['./clazz-details.component.css']
 })
-export class ClazzDetailsComponent {
+export class ClazzDetailsComponent implements OnInit {
+
+  clazz!: Clazz
+
+  currentModal: NgbModalRef | undefined
+
+  constructor(private modalService: NgbModal,
+              private activatedRoute: ActivatedRoute,
+              private clazzService: ClazzService,
+              private router: Router) {
+  }
+
+  DeleteClazz() {
+    if (environment.production) {
+      this.clazzService.delete(this.clazz.id)
+        .subscribe(value => {
+          this.currentModal?.close()
+          this.router.navigateByUrl('/clazzs')
+        })
+    } else {
+      console.log('clazz deleted !');
+    }
+  }
+  ngOnInit(): void {
+    const id = this.activatedRoute.snapshot.paramMap.get('id')
+    if (id) {
+      this.clazzService.getOne(Number(id)).subscribe(clazz => this.clazz = clazz)
+    }
+    console.log(id)
+  }
+
+  open(content: any) {
+    this.currentModal = this.modalService.open(content, {backdrop: 'static'})
+  }
+
 
 }
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  production: false,
-  apiUrl: '',
+  production: true,
+  apiUrl: 'http://localhost:8087/gestionscolaire',
   classroomMock: 'assets/classroom-mock-data.json',
   clazzMock: 'assets/clazz-mock-data.json',
   lessonMock: 'assets/lesson-mock-data.json',
